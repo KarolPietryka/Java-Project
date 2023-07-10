52ec4f4dd280c5da726a2f17b7141616bf05b427
Karol Pietryka <karol.pietryka11@gmail.com>
2023-07-10 08:43:51 +0200

    [Add]Enable h2 db console

--- src/main/java/com/persado/assignment/project/config/SecurityConfig.java
+++ src/main/java/com/persado/assignment/project/config/SecurityConfig.java
@@ -1,10 +1,19 @@
 package com.persado.assignment.project.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.filter.OncePerRequestFilter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -16,5 +25,25 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/**").permitAll()
                 .anyRequest().authenticated()
                 .and().csrf().disable();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/h2-console/**");
+    }
+
+    @Bean
+    public FilterRegistrationBean<Filter> frameOptionsFilter() {
+        FilterRegistrationBean<Filter> filter = new FilterRegistrationBean<>();
+        filter.setFilter(new OncePerRequestFilter() {
+            @Override
+            protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+                if (request.getRequestURI().startsWith("/h2-console")) {
+                    response.setHeader("X-Frame-Options", "SAMEORIGIN");
+                }
+                filterChain.doFilter(request, response);
+            }
+        });
+        return filter;
     }
 }
